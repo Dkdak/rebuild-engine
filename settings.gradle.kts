af871538d9f5095e0e01d3108cf3807ab82f92d0
jason <sleesuengjae79@nate.com>
2026-06-22 11:07:11 +0900

    init rebuild-engine

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "rebuild-engine"
---
+++ src/main/java/com/mteam/rebuildengine/RebuildEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mteam.rebuildengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebuildEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebuildEngineApplication.class, args);
+    }
+
+}
